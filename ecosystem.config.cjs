d6c0a2e6c21c54d777b897090bd8517ebbbb8fe9
Hidalgo Ponce <hidalgoponce@Hidalgos-MacBook-Pro.local>
2024-07-25 11:15:14 -0500

    Configurando servidos pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'NuxtAppName',
+            name: 'gotoecuador',
             port: '6000',
             exec_mode: 'cluster',
             instances: 'max',
